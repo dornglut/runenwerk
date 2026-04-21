86c13946a994020c9f90bd8ea497b0d794781d9d
Joshua Jung || jj050 <128308156+Crystonix@users.noreply.github.com>
2026-04-21 20:57:04 +0200

    scene: tighten replay contract to snapshot-only

--- engine/src/plugins/scene/replay/codec.rs
+++ engine/src/plugins/scene/replay/codec.rs
@@ -45,41 +45,6 @@ pub struct SceneSimulationSnapshotV2 {
     pub entities: SceneEntitySnapshotV2,
 }
 
-#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
-pub struct SceneEntityDeltaV2 {
-    pub frame_counter: Option<domain::WorldFrameCounter>,
-    pub debug_position: Option<domain::WorldDebugPosition>,
-    pub debug_velocity: Option<domain::WorldDebugVelocity>,
-}
-
-#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
-pub struct SceneWorldContextDeltaV2 {
-    pub world: Option<SceneSlot>,
-    pub overlays: Option<Vec<SceneSlot>>,
-    pub world_scene_label: Option<String>,
-    pub overlay_scene_label: Option<String>,
-    pub gameplay_config: Option<domain::GameplayConfig>,
-    pub gameplay_config_modified_millis: Option<Option<u64>>,
-    pub gameplay_config_revision: Option<u64>,
-    pub overlay_consumed: Option<bool>,
-    pub player_move_x: Option<f32>,
-    pub player_move_y: Option<f32>,
-    pub camera_yaw: Option<f32>,
-    pub camera_pitch: Option<f32>,
-    pub camera_distance: Option<f32>,
-    pub delta_seconds: Option<f32>,
-    pub fixed_step_seconds: Option<f32>,
-    pub fixed_step_accumulator: Option<f32>,
-    pub frame_count: Option<u64>,
-    pub enemy_kills: Option<u32>,
-}
-
-#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
-pub struct SceneSimulationDeltaV2 {
-    pub context: SceneWorldContextDeltaV2,
-    pub entities: SceneEntityDeltaV2,
-}
-
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct SceneReplayInputFrameV2 {
     pub tick: SimulationTick,
---
+++ engine/src/plugins/scene/test_support/mod.rs
@@ -0,0 +1,5 @@
+mod simulation_delta;
+
+pub(crate) use simulation_delta::{
+    build_scene_simulation_delta, apply_scene_simulation_delta,
+};
