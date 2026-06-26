e6426d699109c35c91e8d59d36f4549335f16491
Joshua Jung || jj050 <128308156+Crystonix@users.noreply.github.com>
2026-06-26 13:58:42 +0200

    feat(engine): export ui render output proof

--- engine/src/plugins/render/features/ui/mod.rs
+++ engine/src/plugins/render/features/ui/mod.rs
@@ -1,9 +1,11 @@
 pub mod descriptor;
 pub mod prepared;
+pub mod render_output_proof;
 pub mod resource;
 pub mod submission;
 
 pub use descriptor::*;
 pub use prepared::*;
+pub use render_output_proof::*;
 pub use resource::*;
 pub use submission::*;
