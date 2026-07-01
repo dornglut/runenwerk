d9e58814f0bdfbe1cf8ac5bd3bfdf4addc290cb2
Joshua Jung || jj050 <128308156+Crystonix@users.noreply.github.com>
2026-07-01 23:14:45 +0200

    Add overlay stack helpers

---
+++ domain/ui/ui_runtime/src/overlay/stack.rs
@@ -0,0 +1,21 @@
+use ui_controls::{ControlOverlayFocusPolicy, ControlOverlayKind, ControlOverlayRequirement};
+
+use super::MountedOverlayControl;
+
+pub fn overlay_scope(requirement: &ControlOverlayRequirement) -> String {
+    match requirement.kind {
+        ControlOverlayKind::Menu => "scope.menu".to_owned(),
+        ControlOverlayKind::Tooltip => "scope.tooltip".to_owned(),
+        ControlOverlayKind::FocusContainingOverlay => "scope.focus-containing".to_owned(),
+        _ => "scope.anchored".to_owned(),
+    }
+}
+
+pub fn focus_return_anchor(control: &MountedOverlayControl, requirement: &ControlOverlayRequirement) -> Option<String> {
+    match requirement.focus_policy {
+        ControlOverlayFocusPolicy::ReturnToAnchor
+        | ControlOverlayFocusPolicy::ReturnToPrevious
+        | ControlOverlayFocusPolicy::ContainFocus => Some(control.anchor_id.clone()),
+        _ => None,
+    }
+}
