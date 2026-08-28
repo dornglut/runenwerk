059aa36eaa13354543b3bf79cac78f05d891999b
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-08-28 15:17:14 +0200

    gpu: realize prevalidated compute bindings

--- engine/src/plugins/gpu/backend/wgpu/execution.rs
+++ engine/src/plugins/gpu/backend/wgpu/execution.rs
@@ -1962,7 +1962,7 @@ async fn prepare_compute_operation(
             .await
             .map_err(preparation_program_binding_failure)?;
         let realization = context
-            .realize_bind_group(&layout, group.values().cloned())
+            .realize_validated_bind_group(&layout, group.clone())
             .await
             .map_err(preparation_program_binding_failure)?;
         bind_groups.push(PreparedBindGroup {
