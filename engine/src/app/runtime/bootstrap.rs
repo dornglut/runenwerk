d7ab3d844c86020695c8ea21d25efe08e54520c3
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-09-14 00:21:44 +0200

    runtime: remove Execution Fabric resources from bare App

--- engine/src/app/runtime/bootstrap.rs
+++ engine/src/app/runtime/bootstrap.rs
@@ -83,14 +83,6 @@ impl App {
             self.world
                 .insert_resource(QuerySnapshotRuntimeResource::default());
         }
-        if !self.world.has_resource::<RuntimeJobExecutorResource>() {
-            self.world
-                .insert_resource(RuntimeJobExecutorResource::default());
-        }
-        if !self.world.has_resource::<RuntimeProductCacheResource>() {
-            self.world
-                .insert_resource(RuntimeProductCacheResource::default());
-        }
         if !self.world.has_resource::<SimulationProfileConfig>() {
             self.world
                 .insert_resource(SimulationProfileConfig::default());
