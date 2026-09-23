7da154a3d5d356699ad3b5fea9c4503c145609f5
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-09-23 22:16:17 +0200

    test: use public windowed state constructor

--- apps/runenwerk_editor/tests/viewport_gpu_truth_smoke.rs
+++ apps/runenwerk_editor/tests/viewport_gpu_truth_smoke.rs
@@ -118,7 +118,6 @@ fn viewport_gpu_truth_smoke() {
     let mut native_state = WindowState::windowed(window.title());
     native_state.size_px = (size.width, size.height);
     native_state.scale_factor = window.scale_factor();
-    native_state.set_headless(false);
     app.world_mut()
         .resource_mut::<WindowStateRegistryResource>()
         .expect("window registry should exist")
