1fab4068e6bc9f68301d21780da6de968b0df460
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-09-24 12:59:59 +0200

    style: apply rustfmt input provider changes

--- engine/src/plugins/debug_metrics/mod.rs
+++ engine/src/plugins/debug_metrics/mod.rs
@@ -305,9 +305,7 @@ fn debug_overlay_font_atlas() -> &'static UiFontAtlasResource {
 mod tests {
     use super::{DEBUG_METRICS_FRAME_PRODUCER_ID, DebugMetricsPlugin};
     use crate::plugins::render::{RenderReadinessState, SurfaceFrameSubmissionRegistryResource};
-    use crate::plugins::{
-        ActionState, InputFinalizePlugin, InputState, ScenePlugin, TimePlugin,
-    };
+    use crate::plugins::{ActionState, InputFinalizePlugin, InputState, ScenePlugin, TimePlugin};
     use crate::prelude::*;
     use winit::event::ElementState;
     use winit::keyboard::KeyCode;
