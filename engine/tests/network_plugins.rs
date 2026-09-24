16af0eaf0bc479a592c7e630ad3a611acc5ee087
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-09-24 18:08:57 +0200

    [Net] Fix R0 proof include formatting

--- engine/tests/network_plugins.rs
+++ engine/tests/network_plugins.rs
@@ -294,4 +294,6 @@ include!("network_plugins/basic_flow.rs");
 
 include!("network_plugins/runtime_and_replication.rs");
 
-include!("network_plugins/delta_and_reconnect.rs");\n\ninclude!("network_plugins/replicated_view_r0.rs");
+include!("network_plugins/delta_and_reconnect.rs");
+
+include!("network_plugins/replicated_view_r0.rs");
