e54ba83c283f94807820d684b02e108e30e27535
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-09-24 18:08:26 +0200

    fix(render): import runtime texture view dimension

--- engine/src/plugins/render/renderer/render_flow/runtime_resources.rs
+++ engine/src/plugins/render/renderer/render_flow/runtime_resources.rs
@@ -8,7 +8,8 @@ use crate::plugins::render::{
 use runen_gpu::{
     GpuBufferDescriptor, GpuBufferHandle, GpuBufferUsages, GpuTextureDescriptor,
     GpuTextureDimension, GpuTextureFormat, GpuTextureHandle, GpuTextureUsages,
-    GpuTextureViewHandle, GpuWorkResourceId, GpuWorkResourceIdAllocator, PreparedGpuData,
+    GpuTextureViewDimension, GpuTextureViewHandle, GpuWorkResourceId, GpuWorkResourceIdAllocator,
+    PreparedGpuData,
     UniformData,
 };
 use std::fmt;
