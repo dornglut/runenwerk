7bd6b31ba1f1d10dc0647913f2225d2e4f8568c0
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-09-25 13:26:53 +0200

    Apply rustfmt to authority replication cut

--- net/engine_net/src/prelude.rs
+++ net/engine_net/src/prelude.rs
@@ -2,8 +2,7 @@ pub use crate::protocol::*;
 pub use crate::replication::{
     InputDriver, NetEntityMap, NetEntityMapEvent, ReplicationDriver, ReplicationStats,
     SnapshotApplyDriver, SnapshotCursor, SnapshotTimeline, apply_delta_payload, delta_debug_dump,
-    normalize_delta_payload,
-    snapshot_debug_dump,
+    normalize_delta_payload, snapshot_debug_dump,
 };
 pub use crate::simulation::*;
 pub use crate::{
